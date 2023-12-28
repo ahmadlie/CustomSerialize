844ccf54875d95dfc348b005b790354f3042759e
ahmadlie <eldar.ehmedli2003@mail.ru>
2023-12-28 09:29:08 -1200

    Add project files.

---
+++ CustomSerializeDeserialize/CustomSD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSerializeDeserialize
+{
+    public static class CustomSD
+    {
+        public static string Serialize<T>(T myClass) where T : class
+        {
+            var typeName = typeof(T).Name;
+
+            string jsonString = $"\"{typeName}\":" + " {\n";
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                var propValue = prop.GetValue(myClass).ToString();
+                var propName = prop.Name;
+                
+                if (prop.PropertyType.Name == "String")
+                {
+                    jsonString += $"  \"{propName}\": \"{propValue}\"\n";
+                }
+                else
+                {
+                    jsonString += $"  \"{propName}\": {propValue}\n";
+
+                }
+            }
+            return jsonString + "}";
+        }
+    }
+}
---
+++ CustomSerializeDeserialize/Program.cs
@@ -0,0 +1,37 @@
+﻿using CustomSerializeDeserialize;
+
+Employee employee = new Employee();
+employee.Name = "Eldar";
+employee.Salary = 1000;
+employee.Married = false;
+
+Animal animal = new Animal();
+animal.Name = "Exapmle Dog";
+animal.Age = 5;
+
+string jsonString1 = CustomSD.Serialize(employee);
+string jsonString2 = CustomSD.Serialize(animal);
+
+Console.WriteLine(jsonString1);
+Console.WriteLine("********************");
+Console.WriteLine(jsonString2);
+
+
+
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public bool Married { get; set; }
+}
+
+public class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+}
+
+
+
